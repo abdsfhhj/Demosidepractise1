438c5770acad05dd59272479bcd7216d99cae902
user <user@Dell-pc>
2022-12-26 09:47:05 +0530

    locator is best

---
+++ Demosidepractise1/src/test/java/Locator/demo.java
@@ -0,0 +1,19 @@
+package Locator;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class demo {
+	
+	public static void main(String[]args)
+	
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+}
